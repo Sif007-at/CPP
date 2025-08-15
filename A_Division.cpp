8531f0ea7f07e0097455ad274e64482fea990117
MD ABU YUSUF SIFAT <abuyusufsifat007@gmail.com>
2025-08-16 04:18:56 +0600

    Create A_Division.cpp

---
+++ A_Division.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+     using namespace std;
+        int main(){
+        int f;
+              cin>>f;
+             while(f--){
+        int x;
+              cin>>x;
+        if(n<=1399) { cout<<"Division 4"<<endl;
+                    }else if(x<=1599) {cout<<"Division 3"<<endl;
+                                      }else if(x<=1899) {cout<<"Division 2"<<endl;
+                                                        }else {cout<<"Division 1"<<endl;}
+    }
+      return 0;
+}
